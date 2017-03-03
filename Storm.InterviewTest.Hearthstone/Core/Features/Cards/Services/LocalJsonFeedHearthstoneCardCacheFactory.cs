d33f98626e7194d65cbe577aa23981b1b589fe07
Nick Spicer <nickspicer93@gmail.com>
2017-03-03 21:11:05 +0000

    [Task5] Refactor LocalJsonFeedHearthstoneCardCacheFactory
    Retrieve the separate arrays via lookups then use the .ParseArray to do
    most of the work. I believe this would ideally be best as a DTO if the
    JSON was restructured.

--- Storm.InterviewTest.Hearthstone/Core/Features/Cards/Services/LocalJsonFeedHearthstoneCardCacheFactory.cs
+++ Storm.InterviewTest.Hearthstone/Core/Features/Cards/Services/LocalJsonFeedHearthstoneCardCacheFactory.cs
@@ -3,6 +3,10 @@ using System.IO;
 using System.Web;
 using Newtonsoft.Json.Linq;
 using Storm.InterviewTest.Hearthstone.Core.Features.Cards.Domain;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Reflection;
+using System;
 
 namespace Storm.InterviewTest.Hearthstone.Core.Features.Cards.Services
 {
@@ -12,57 +16,20 @@ namespace Storm.InterviewTest.Hearthstone.Core.Features.Cards.Services
 		{
 		}
 
-		protected override IEnumerable<ICard> PopulateCards(IHearthstoneCardParser parser)
+        protected override IEnumerable<ICard> PopulateCards(IHearthstoneCardParser parser)
 		{
-			using (var reader = File.OpenText(HttpContext.Current.Server.MapPath("~/App_Data/cards.json")))
-			{
-				var cardSets = JObject.Parse(reader.ReadToEnd());
-				JToken cards;
-				if (cardSets.TryGetValue("Basic", out cards) && cards.Type == JTokenType.Array)
-				{
-					foreach (var card in cards)
-					{
-						var parsedCard = parser.Parse(card.ToString());
-						if (parsedCard != null)
-						{
-							yield return parsedCard;
-						}
-					}
-				}
-				if (cardSets.TryGetValue("Classic", out cards) && cards.Type == JTokenType.Array)
-				{
-					foreach (var card in cards)
-					{
-						var parsedCard = parser.Parse(card.ToString());
-						if (parsedCard != null)
-						{
-							yield return parsedCard;
-						}
-					}
-				}
-				if (cardSets.TryGetValue("Curse of Naxxramas", out cards) && cards.Type == JTokenType.Array)
-				{
-					foreach (var card in cards)
-					{
-						var parsedCard = parser.Parse(card.ToString());
-						if (parsedCard != null)
-						{
-							yield return parsedCard;
-						}
-					}
-				}
-				if (cardSets.TryGetValue("Goblins vs Gnomes", out cards) && cards.Type == JTokenType.Array)
-				{
-					foreach (var card in cards)
-					{
-						var parsedCard = parser.Parse(card.ToString());
-						if (parsedCard != null)
-						{
-							yield return parsedCard;
-						}
-					}
-				}
-			}
+            // My preferred way to do this would be a DTO class but found that quite difficult 
+            // as what we need to query ends up as property names, alternatively we could also use a JsonConvert.DeserialiseObject with a customer converter but I couldn't get that to work either.
+
+            JToken cardSetsAsJson;
+            List<ICard> cards = new List<ICard>();
+            using (var reader = File.OpenText(HttpContext.Current.Server.MapPath("~/App_Data/cards.json")))
+                cardSetsAsJson = JToken.Parse(reader.ReadToEnd());
+
+            foreach (var cardSet in new[] { cardSetsAsJson["Basic"], cardSetsAsJson["Curse of Naxxramas"], cardSetsAsJson["Classic"], cardSetsAsJson["Goblins vs Gnomes"] })
+                cards.AddRange(parser.ParseArray(cardSet.ToString()));
+
+            return cards;
 		}
 	}
 }
